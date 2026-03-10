31b84694b1c60b792ec3f2e6a367e8157ea709a5
Akarsh Gyan <akarsh02.com@gmail.com>
2026-03-10 10:19:56 +0530

    Akarsh Added UC2

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -4,6 +4,7 @@ public class PalindromeCheckerApp {
             String rev = "";
 
             for(int i = s.length() - 1; i >= 0; i--) {
+
                 rev = rev + s.charAt(i);
             }
 
