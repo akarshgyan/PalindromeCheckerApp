333a012565b86ec1a3ee5c7152a3eb403a30934e
Akarsh Gyan <akarsh02.com@gmail.com>
2026-03-10 10:01:24 +0530

    Akarsh Added UC1

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,8 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Palindrome Checker App");
+        System.out.println("UC1: Application Entry & Welcome Message");
+        System.out.println("Version: 1.0");
+        System.out.println("Welcome to the Palindrome Checker Application");
+    }
+}
